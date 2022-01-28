3f9440d40828adc8f8aca821e8df7cbfd5bcec96
ca1e <i@scp500.com>
2022-01-28 17:51:37 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ECP
+
+## 脚本说明
+
+- [文档](Script.md)
+
+## 虚拟机指令说明
+
+- [VM1](VM1.md)
+- [VM2](VM2.md)
+
+### ref
+
+- https://github.com/bajdcc/CMiniLang
+- https://github.com/lotabout/write-a-C-interpreter
+- https://github.com/rswier/c4
+- https://github.com/pandolia/tinyc
---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+var text = "";
+if(args.Length > 1)
+{
+    text = File.ReadAllText(args[1]);
+}
+
+const string Ident = @"[\d\p{L}_]+";
+const string Instant = @"-?\d+";
+const string Constant = @"_"+Ident;
+const string VarStant = @"\${1,2}"+Ident;
+const string ExtVar = @"@"+Ident;
+
+Console.WriteLine($"code: <{text}>");
