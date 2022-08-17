364b3a820855423e0ee14138ae124f946a158bd0
ca1e <i@scp500.com>
2022-08-17 18:03:41 +0800

    fix

--- src/ScriptUtil.cs
+++ src/ScriptUtil.cs
@@ -11,9 +11,22 @@ static class ScriptUtil
         ECP.Parser.ECParser.OnDefineLexer(lexer);
         text = compat(text);
 
+        List<Compiler.Scanners.Lexeme> tokens = new();
+
         foreach(var t in lexer.Parse(text))
         {
-            Console.WriteLine(t);
+            if(t.Tag == ECP.Parser.ECParser.K_IF)
+            {
+                Console.WriteLine($"is if:{t}");
+            }
+            if(t.Tag == ECP.Parser.ECParser.T_NEWLINE)
+            {
+                Console.WriteLine($"---------------------");
+            }
+            else
+            {
+                Console.WriteLine(t);
+            }
         }
     }
 
@@ -23,16 +36,22 @@ static class ScriptUtil
         var lines = Regex.Split(text, "\r\n|\r|\n");
         foreach (var line in lines)
         {
-            text = line.Trim();
-            if(text == string.Empty)continue;
-            var mp = Regex.Match(text, @"^print\s+(.*)$", RegexOptions.IgnoreCase);
+            var _text = line.Trim();
+            var m = Regex.Match(_text, @"(\s*#.*)$");
+            if (m.Success)
+            {
+                var comment = m.Groups[1].Value;
+                _text = line[..^comment.Length];
+            }
+            if(_text == string.Empty)continue;
+            var mp = Regex.Match(_text, @"^print\s+(.*)$", RegexOptions.IgnoreCase);
             if (mp.Success)
             {
                 builder.Append($"print \"{mp.Groups[1].Value}\"");
             }
             else
             {
-                builder.Append(text);
+                builder.Append(_text);
             }
             builder.Append("\r\n");
         }
