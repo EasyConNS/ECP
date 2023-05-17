43b832216971fee8ac8e85d8951a652a99bbd74d
ca1e <i@scp500.com>
2023-05-17 16:19:43 +0800

    update test

--- src/ScriptUtil.cs
+++ src/ScriptUtil.cs
@@ -11,32 +11,74 @@ static class ScriptUtil
         CompilationErrorList errorList = errorManager.CreateErrorList();
         var scripter = new ECP.VBFECScript(errorManager);
         scripter.Initialize();
-        var ast = scripter.Parse(text, errorList);
-        foreach(var s in ast.Statements)
-        {
-            Console.Writeline(s);
-        }
+        var ast = scripter.Parse(compat(text), errorList);
+        if(errorList.Count != 0)
+                {
+                    foreach(var err in errorList)
+                    {
+                        Console.WriteLine(err);
+                    }
+                }
+        var visitor = new SimpleVisitor(errorManager);
+        visitor.Visit(ast);
     }
 
     private static string compat(string text)
-    {
-        var builder = new StringBuilder();
-        var lines = Regex.Split(text, "\r\n|\r|\n");
-        foreach (var line in lines)
         {
-            var _text = line.Trim();
-            //if(_text == string.Empty)continue;
-            var mp = Regex.Match(_text, @"^print\s+(.*)$", RegexOptions.IgnoreCase);
-            if (mp.Success)
+            var builder = new StringBuilder();
+            foreach (var line in Regex.Split(text, "\r\n|\r|\n"))
             {
-                builder.Append($"PRINT \"{mp.Groups[1].Value}\"");
+                var _text = line.TrimStart();
+                var m = Regex.Match(_text, @"(\s*#.*)$");
+                string comment = string.Empty;
+                if (m.Success)
+                {
+                    comment = m.Groups[1].Value;
+                    _text = _text[..^comment.Length];
+                }
+                _text = _text.Trim();
+                
+                var mp = Regex.Match(_text, @"^print\s+(.*)$", RegexOptions.IgnoreCase);
+                if (mp.Success)
+                {
+                    builder.Append($"PRINT \"{mp.Groups[1].Value}\";");
+                }
+                else
+                {
+                    builder.Append(_text);
+                    if (_text.StartsWith("for", true, null))
+                    {
+                        builder.Append(':');
+                    }
+                    else if(_text.StartsWith("if", true, null))
+                    {
+                        builder.Append(':');
+                    }
+                    else if (_text.StartsWith("elif", true, null))
+                    {
+                        builder.Append(':');
+                    }
+                    else if (_text.StartsWith("else", true, null))
+                    {
+                        builder.Append(':');
+                    }
+                    else if (_text.StartsWith("function", true, null))
+                    {
+                        builder.Append(':');
+                    }
+                    else if (_text == String.Empty)
+                    {
+                        // ignore empty line
+                    }
+                    else
+                    {
+                        builder.Append(';');
+                    }
+                }
+
+                builder.Append(comment);
+                builder.Append("\r\n");
             }
-            else
-            {
-                builder.Append(_text);
-            }
-            builder.Append("\r\n");
+            return builder.ToString();
         }
-        return builder.ToString();
-    }
 }
